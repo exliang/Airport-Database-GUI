6a14fbc3542574ee1b11affec6cbae302421e8eb
exliang <156244062+exliang@users.noreply.github.com>
2024-12-19 15:42:15 -0800

    Update events.py

--- p2app/views/events.py
+++ p2app/views/events.py
@@ -1,16 +1,7 @@
-# p2app/views/events.py
-#
-# ICS 33 Spring 2024
-# Project 2: Learning to Fly
-#
 # These are a collection of events that are internal to the user interface.
 # When the user interface sends these events, they are propagated to other
 # components within the user interface, but aren't sent to the engine to
 # be processed by it.
-#
-# YOU WILL NOT NEED TO MODIFY THIS FILE AT ALL
-
-
 
 def is_internal_event(event):
     return hasattr(event, '_INTERNAL')
