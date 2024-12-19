52936d3d86c1272e87edea0ae61e390f81fe1f11
exliang <156244062+exliang@users.noreply.github.com>
2024-12-19 15:32:56 -0800

    Update app.py

--- p2app/events/app.py
+++ p2app/events/app.py
@@ -1,17 +1,3 @@
-# p2app/events/app.py
-#
-# ICS 33 Spring 2024
-# Project 2: Learning to Fly
-#
-# Application-level events that are either sent by the user interface to the
-# engine, or from the engine back to the user interface.
-#
-# See the project write-up for details on when these events are sent and by whom.
-#
-# YOU WILL NOT NEED TO MODIFY THIS FILE AT ALL
-
-
-
 class ErrorEvent:
     def __init__(self, message: str):
         self._message = message
