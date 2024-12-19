5825568c87d130de375c65cf86765be6bdf048b3
exliang <156244062+exliang@users.noreply.github.com>
2024-12-19 15:41:29 -0800

    Update empty.py

--- p2app/views/empty.py
+++ p2app/views/empty.py
@@ -1,11 +1,4 @@
-# p2app/views/empty.py
-#
-# ICS 33 Spring 2024
-# Project 2: Learning to Fly
-#
 # An empty user interface area, for use when the application first starts up.
-#
-# YOU WILL NOT NEED TO MODIFY THIS FILE AT ALL
 
 import tkinter
 
